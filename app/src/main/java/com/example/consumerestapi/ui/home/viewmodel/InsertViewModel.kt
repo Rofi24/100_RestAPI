871545d97d35ca4d0e2f2baa15b47b3cf5976c49
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2023-12-27 18:23:01 +0700

    Membuat class InsertViewModel dengan dependensi KontakRepository yang meng-extend ViewModel

---
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.consumerestapi.ui.home.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.consumerestapi.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){}
